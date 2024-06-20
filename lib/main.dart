599848552e5e3d0e64d17cf478d58ed1ca2a8fb5
ikawlangsapatna143 <bansonchristoper@gmail.com>
2024-06-20 12:16:57 +0800

    Activity_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner:false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text (
-            'SARAP BIIIII',
-            style: TextStyle(
-                fontSize:30.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Raleway',
-                color: Colors.black
-            ),
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Scaffold(
+    appBar: AppBar(
+      title: Center(
+        child: Text (
+          'User Profile',
+          style: TextStyle(
+              fontSize:30.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Raleway',
+              color: Colors.white
           ),
-          ),
-            backgroundColor: Colors.red[600],
-          ),
-        body: Center(
-          child: Image.network('https://img-9gag-fun.9cache.com/photo/azmjEpN_700bwp.webp'),
-         ),
-      floatingActionButton:FloatingActionButton(
-        onPressed: () {},
-       backgroundColor: Colors.red[100],
-        child: Icon(
-         Icons.add,
-          color: Colors.white,
         ),
       ),
+      backgroundColor: Colors.deepPurple[600],
     ),
-  ));
-}
+  body: Padding(
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+            'Name:',
+           style: TextStyle(
+           fontSize: 18.0,
+           color: Colors.black,
+    ),
+        ),
+        Text(
+          'CHRISTOPER NEO G. BANSON',
+          style: TextStyle(
+              fontSize:20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Raleway',
+              color: Colors.black
+          ),
+        ),
+        SizedBox(height: 30.0,),
+        Text(
+          'Year:',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.black,
+          ),
+        ),
+        Text(
+          '3RD YEAR COLLEGE',
+          style: TextStyle(
+              fontSize:20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Raleway',
+              color: Colors.black
+          ),
+        ),
+        SizedBox(height: 30.0,),
+        Text(
+          'Email:',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.black,
+          ),
+        ),
+        Text(
+          'BANSONCHRISTOPER@GMAIL.COM',
+          style: TextStyle(
+              fontSize:20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Raleway',
+              color: Colors.black
+          ),
+        ),
+      ],
+    ),
 
+  ),
+  ),
+));
